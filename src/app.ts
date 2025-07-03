1ccac7dbb49ba6fed3148c2a2125082a96340a09
nibirnafis <nibirnafis@gmail.com>
2025-07-03 12:31:08 +0600

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Application, Request, Response } from "express"
+import cors from 'cors'
 import { Book } from "./app/models/Book.model"
 import { bookRoutes } from "./app/controlers/BookControlers"
 
 const app: Application = express()
 
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 app.use(express.json())
 app.use("/", bookRoutes)
+
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World")
