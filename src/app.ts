8f4e10f8d855702e32bee195bd2519c5f7ea81a1
nibirnafis <nibirnafis@gmail.com>
2025-07-03 13:10:00 +0600

    fixed issues

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express, { Application, Request, Response } from "express"
 import cors from 'cors'
-import { Book } from "./app/models/Book.model"
 import { bookRoutes } from "./app/controlers/BookControlers"
 
 const app: Application = express()
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "http://localhost:5173", credentials: true
 }))
 app.use(express.json())
 app.use("/", bookRoutes)
